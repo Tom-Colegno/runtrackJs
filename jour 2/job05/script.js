1cd2e5c66e94e0951cf8e58232ca73bddc439105
tom-colegno <91758466+Tom-Colegno@users.noreply.github.com>
2024-03-26 15:12:36 +0100

    Delete jour 2 directory

--- jour 2/job05/script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('scroll', function() {
-    var scrollPercent = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
-    document.querySelector('footer').style.backgroundColor = `hsl(${scrollPercent}, 100%, 50%)`;
-  });
